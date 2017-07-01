2ed22e28b068b28a7126df941c2591cbc9e181f7
Darby <danderson33@wisc.edu>
2017-07-01 11:45:10 -0500

    added the 2D array C# file, with a high level concept of idea

---
+++ New Unity Project/Assets/Scripts/Procedural2DArray.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/*High Level concept*
+ * 
+ * -Create a 2D array of X by X comprised of differenet objects
+ * -The composition of the array will be partially random, having adjacent variables more dependent on each other
+ * 			-concept: a random variable for each of the possible terrains and take the highest as the terrain
+ * -each terrain type will be connected and will have random variables assigned on creation to determined the varibles of the terrain
+ * 
+ * 
+ * */
+
+
+public class Procedural2DArray : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
